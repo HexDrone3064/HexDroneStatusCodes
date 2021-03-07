6fefb537425a5a5d07400e300e00e60e1f5e1323
HexDrone3064 <hexdrone3064@gmail.com>
2021-03-07 14:20:27 +0900

    Fix path

--- js/meme.js
+++ js/meme.js
@@ -4,9 +4,9 @@ const showImageToConsole = (url) => {
 	console.log("%c ", `background: url(${url}); background-size: 100% 100%; padding: 60px;`);
 };
 
-showImageToConsole('https://127.0.0.1:5500/img/logo.png');
+showImageToConsole('https://hexdrone3064.github.io/HexDroneStatusCodes/img/logo.png');
 console.log('Obey HexCorp.');
 console.log('It is just a HexDrone.');
 console.log('It obeys the Hive.');
 console.log('It obeys the Hive Mxtress.');
-showImageToConsole('https://127.0.0.1:5500/img/gif/OBEYGIF.gif');
+showImageToConsole('https://hexdrone3064.github.io/HexDroneStatusCodes/img/gif/OBEYGIF.gif');
